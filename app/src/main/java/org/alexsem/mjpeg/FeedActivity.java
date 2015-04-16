66592bc4546a2504da9367c2346fe303f665d2d4
Alexandr Semeniuk <alexandr.semeniuk@gmail.com>
2015-04-16 15:25:55 +0300

    Immersive mode

--- app/src/main/java/org/alexsem/mjpeg/FeedActivity.java
+++ app/src/main/java/org/alexsem/mjpeg/FeedActivity.java
@@ -19,6 +19,14 @@ public class FeedActivity extends Activity {
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+		getWindow().getDecorView().setSystemUiVisibility(
+				View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+						| View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+						| View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+						| View.SYSTEM_UI_FLAG_HIDE_NAVIGATION // hide nav bar
+						| View.SYSTEM_UI_FLAG_FULLSCREEN // hide status bar
+						| View.SYSTEM_UI_FLAG_IMMERSIVE);
+
 		generateLayout(getSharedPreferences(getPackageName(), MODE_PRIVATE).getInt("cameras", 4));
 	}
 
